f06fc091178da2f539a59ae6f9eaf768d4cb366d
kerim2022 <kerimsha@gmail.com>
2022-12-07 07:41:54 -0800

    loop tasks

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numbers in range(0, 10):
+	for number in range(numbers + 1, 10):
+		if numbers == 8 and number == 9:
+			print("{}{}".format(numbers, number))
+		else:
+			print("{}{}".format(numbers, number), end=", ")
