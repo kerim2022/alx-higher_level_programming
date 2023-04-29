0a13cdc650f405baa03219c0dfedec602cca233a
Abdulkerim <kerimsha@gmail.com>
2023-04-29 12:56:53 +0000

    correction of wrong otput

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
-
-if (process.argv.length <= 3) {
+const myArgs = process.argv.slice(2);
+if (!myArgs[0] || !myArgs[2]) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const args = myArgs.map(Number);
+  const firstMax = Math.max.apply(null, args);
+  args.splice(args.indexOf(firstMax), 1);
+  console.log(Math.max.apply(null, args));
 }
